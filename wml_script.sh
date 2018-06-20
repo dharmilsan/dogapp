887490c093c4149bd504c520576b105cbac4cfd8
Dharmil <dharmil.shah@aasaanjobs.com>
2018-06-20 15:49:29 +0530

    Update wml_script to not try to kill any process if wml has not been started

--- wml_script.sh
+++ wml_script.sh
@@ -1,2 +1,10 @@
-ps aux | grep [w]ml | head -1 | awk {'print $2'} | xargs kill -9
+WML_PID="$(ps aux | grep [w]ml | head -1 | awk {'print $2'})"
+
+if [ -z "$WML_PID" ]; then
+  echo "${WML_PID} wml is not running"
+elif [ -n "$WML_PID" ]; then
+  kill -9 "${WML_PID}"
+  echo "${WML_PID} wml pid has been killed"
+fi
+
 wml start&
